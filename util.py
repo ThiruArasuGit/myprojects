67ff00231f8021dadfc7815d4bbbfd8bb8f2f6ca
Thiru A <thirunavukkarasu143@gmail.com>
2020-12-31 11:07:39 +0530

    Added more random utils

--- util.py
+++ util.py
@@ -1,5 +1,16 @@
 import os,random
 
 num = random.random()
-print(num)
+print("Random number in float: " + str(num))
 
+num1to100 = random.randint(1,100)
+print("num1to100: " + str(num1to100))
+
+num100to1000 = random.randint(100,1000)
+print("num100to1000: " + str(num100to1000))
+
+randrange = random.randrange(1,10)
+print ("randrange: " + str(randrange))
+
+randchoice = random.choice('computer')
+print("randchoice: " + randchoice)
